75a20e372079fb84bdb3a9ed8994fa8ed8887c44
Christina Steinhauer <christina.spost@gmail.com>
2023-05-04 17:33:24 +0200

    f u

--- python-server/server.py
+++ python-server/server.py
@@ -14,28 +14,6 @@ name = "american_idiot"
 def sources():
     return seperator.sources()
 
-@app.route("/chunk", methods=["GET"])
-def chunk():
-    # filepath = PATH + name;
-
-    # # checks if file exists
-    # if os.path.isfile(filepath + '.json') and os.access(PATH, os.R_OK):
-    #     print ("File exists and is readable")
-
-    #     f = open(filepath + '.json')
-    #     output = json.loads(f.read())
-    # else:
-    output = seperator.run(filepath + '.wav')
-
-        # # Serializing json
-        # json_object = json.dumps(output, indent=4)
-        
-        # # Writing to sample.json
-        # with open(filepath + ".json", "w") as outfile:
-        #     outfile.write(json_object)
-
-    return output
-
 
 @app.route("/file/<source>", methods=["GET"])
 def file(source):
